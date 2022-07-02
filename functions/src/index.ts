c04f1109b51e7d1ecb2aaa749aabe5cc2f587497
Joel Kingsley <joelkingsley.r@gmail.com>
2022-07-03 02:01:42 +0530

    Added firebase function to add hasura custom claims after user creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,30 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp(functions.config().firebase);
+
+// On sign up.
+exports.processSignUp = functions.auth.user().onCreate(user => {
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid
+    }
+  };
+
+  return admin
+    .auth()
+    .setCustomUserClaims(user.uid, customClaims)
+    .then(() => {
+      // Update real-time database to notify client to force refresh.
+      const metadataRef = admin.database().ref("metadata/" + user.uid);
+      // Set the refresh time to the current UTC timestamp.
+      // This will be captured on the client to force a token refresh.
+      return metadataRef.set({ refreshTime: new Date().getTime() });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
