a14b7f1bb448b683eae9c6b9335620c427c74eee
Joel Kingsley <joelkingsley.r@gmail.com>
2022-07-03 20:29:06 +0530

    Fixed linting errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,26 +12,23 @@ import * as admin from "firebase-admin";
 admin.initializeApp(functions.config().firebase);
 
 // On sign up.
-exports.processSignUp = functions.auth.user().onCreate(user => {
+exports.processSignUp = functions.auth.user().onCreate(async (user) => {
   const customClaims = {
     "https://hasura.io/jwt/claims": {
       "x-hasura-default-role": "user",
       "x-hasura-allowed-roles": ["user"],
-      "x-hasura-user-id": user.uid
-    }
+      "x-hasura-user-id": user.uid,
+    },
   };
 
-  return admin
-    .auth()
-    .setCustomUserClaims(user.uid, customClaims)
-    .then(() => {
-      // Update real-time database to notify client to force refresh.
-      const metadataRef = admin.database().ref("metadata/" + user.uid);
-      // Set the refresh time to the current UTC timestamp.
-      // This will be captured on the client to force a token refresh.
-      return metadataRef.set({ refreshTime: new Date().getTime() });
-    })
-    .catch(error => {
-      console.log(error);
+  try {
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+    // Update real-time database to notify client to force refresh.
+    const metadataRef = admin.database().ref("metadata/" + user.uid);
+    return await metadataRef.set({
+      refreshTime: new Date().getTime(),
     });
+  } catch (error) {
+    console.log(error);
+  }
 });
