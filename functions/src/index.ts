d3ff09fc6244699f11390a477f83d97b7b8e4c7b
Joel Kingsley <joelkingsley.r@gmail.com>
2022-07-28 01:56:35 +0530

    Moved graphql service inside index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,13 +22,8 @@ exports.processSignUp = functions.auth.user().onCreate(async (user) => {
   };
 
   if (user.email && user.displayName && user.uid) {
-    insertUser(user.email, user.displayName, user.uid)
-      .then(({ data, errors }) => {
-        if (errors) {
-          console.error(errors);
-        }
-        console.log(data);
-      })
+    const hasuraGraphQLService = new HasuraGraphQLService();
+    hasuraGraphQLService.insertUser(user.email, user.displayName, user.uid)
       .then(() => {
         return admin.auth().setCustomUserClaims(user.uid, customClaims);
       })
@@ -44,3 +39,45 @@ exports.processSignUp = functions.auth.user().onCreate(async (user) => {
       });
   }
 });
+
+class HasuraGraphQLService {
+  adminKey = "Yr1mw58ZZaU2kva6JNTdcqOzUj233TxxkUJYMcTzgvb2YHtIzmDj81MWlmhCpn8v";
+
+  async fetchGraphQL(
+    operationsDoc: string,
+    operationName: string,
+    variables: Record<string, any>
+  ) {
+    const nodeFetch = await import("node-fetch");
+    const result = await nodeFetch.default("https://sdo-hasura.hasura.app/v1/graphql", {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables,
+        operationName,
+      }),
+      headers: {
+        "x-hasura-admin-secret": this.adminKey,
+      },
+    });
+    return await result.json();
+  }
+
+  operation = `
+  mutation InsertUser($userEmail: String!, $userName: String!, $userUuid: String!) {
+    insert_users_one(object: {user_email: $userEmail, user_name: $userName, user_uuid: $userUuid}) {
+      user_email
+      user_name
+      user_uuid
+    }
+  }
+`;
+
+  insertUser(userEmail: string, userName: string, userUuid: string) {
+    return this.fetchGraphQL(
+      this.operation,
+      "InsertUser",
+      { "userEmail": userEmail, "userName": userName, "userUuid": userUuid }
+    );
+  }
+}
